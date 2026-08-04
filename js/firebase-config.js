bb721caab217a7f5ad06ca0175bc497aab624041
24022112-png <24022112@vnu.edu.vn>
2026-08-04 21:48:02 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,11 @@
-// Cấu hình Firebase
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBIBD8xulzODI0Goz005FqYU0gm2VKMVpU",
+  authDomain: "olympia-quiz.firebaseapp.com",
+  databaseURL: "https://olympia-quiz-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "olympia-quiz",
+  storageBucket: "olympia-quiz.firebasestorage.app",
+  messagingSenderId: "1005473570114",
+  appId: "1:1005473570114:web:34dac8abaa89163b1b7cdb"
 };
 
 // Khởi tạo Firebase SDK v8
